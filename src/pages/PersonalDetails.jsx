b800dc09a29fd0936fb0fb77a6813e82e7e0c940
Nabal <Knaval10>
2024-07-29 14:10:48 +0545

    contents updated on personal details

--- src/pages/PersonalDetails.jsx
+++ src/pages/PersonalDetails.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const PersonalDetails = () => {
-  return <div>This is the detail page for personal things.</div>;
+  return (
+    <div>
+      This is the detail page for personal things.
+      <div className="">
+        Here, details of every personal card will be displayed
+      </div>
+    </div>
+  );
 };
 
 export default PersonalDetails;
